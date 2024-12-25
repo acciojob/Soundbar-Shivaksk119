59a44cee06025f54dfdda9c50298d83b2bd553a0
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-26 01:01:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ let buttons = document.querySelectorAll('.btn');
 
 buttons.forEach((button)=>{
 	button.addEventListener('click',()=>{
-		let track = button.innerText;
-		let audio = new Audio(`./sounds/${track}.mp3`);
-		audio.play();
+		let track = button.getAttribute('data-sound');
+		let audio = document.getElementById(track);
+		if(audio.paused){
+			audio.play();
+		}
+		else{
+			audio.pause();
+			audio.currentTime = 0;
+		}
 	})
 })
