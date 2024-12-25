7493db7253a8517539d074f0d11a0e60858057fb
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-26 00:11:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((button)=>{
+	button.addEventListener('click',()=>{
+		let track = button.innerText;
+		let audio = new Audio(`./sounds/${track}`);
+		audio.play();
+	})
+})
